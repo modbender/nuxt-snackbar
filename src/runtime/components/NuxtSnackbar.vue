c2a68114c600a401d3a1bcc67fb1aade2d8201f6
modbender <rameshmamathayashas@gmail.com>
2023-04-07 19:30:57 +0530

    remove logging

--- src/runtime/components/NuxtSnackbar.vue
+++ src/runtime/components/NuxtSnackbar.vue
@@ -7,5 +7,4 @@ import { useNuxtApp } from "#imports";
 import { Vue3Snackbar } from "vue3-snackbar";
 
 const snackbarOptions = useNuxtApp()?.$snackbarOptions || {};
-console.log(useNuxtApp(), snackbarOptions);
 </script>
