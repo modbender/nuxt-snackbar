5a79301e29551d712180479855086ccbe8b29149
modbender <{ID}+{username}@users.noreply.github.com>
2024-07-26 01:35:12 +0530

    chore: remove local composable

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,16 +1,3 @@
 # Changelog
 
 
-## v1.1.0
-
-[compare changes](https://undefined/undefined/compare/v1.0.3...v1.1.0)
-
-
-### 🚀 Enhancements
-
-  - Slots (dbede31)
-
-### ❤️  Contributors
-
-- YRM ([@yrming](http://github.com/yrming))
-
--- playground/app.vue
+++ playground/app.vue
@@ -2,13 +2,11 @@
   <main>
     Main Content
     <button @click="test">Open Snackbar</button>
+    <NuxtSnackbar />
   </main>
-  <NuxtSnackbar />
 </template>
 
 <script setup>
-import { useSnackbar } from '#imports';
-
 const snackbar = useSnackbar();
 
 const test = () => {
